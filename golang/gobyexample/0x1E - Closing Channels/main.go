d6c38c46d8f95900a700eae8d6fd5cbfed4353fc
Kairat Amanzharov <Kairat_Amanzharov@epam.com>
2021-07-15 11:00:43 +0600

    Complete Closing Channels Example

---
+++ golang/gobyexample/0x1E - Closing Channels/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				break
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<-done
+}
