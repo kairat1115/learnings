c52f6e7105cc7771ebafa9417ca4db49b2c580ff
Kairat Amanzharov <Kairat_Amanzharov@epam.com>
2021-12-22 14:26:05 +0600

    quii.gitbook.io - fund - mocking refactor

--- golang/quii.gitbook.io/mocking/countdown.go
+++ golang/quii.gitbook.io/mocking/countdown.go
@@ -21,6 +21,15 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
